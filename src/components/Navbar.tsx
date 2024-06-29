4af5dc4d28634a4872129e04eabc4d52ae841403
roodbaraky <kooroshr@hotmail.com>
2024-06-29 20:18:42 +0100

    fixed navbar / container display conflict

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import NavDrawer from "./NavDrawer";
 
 export default function Navbar() {
   return (
-    <nav className="navbar bg-base-100 w-full fixed mx-auto">
+    <nav className="navbar bg-base-100 w-full absolute mx-auto">
       <div className="flex-1">
         <a className="btn btn-ghost text-xl">nafas</a>
       </div>
